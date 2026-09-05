6d311d9b9af9b60c18536715fc51c03be14dde68
Pratham Arora <prathamarora9f15@gmail.com>
2026-09-05 12:38:26 +0300

    feat(api): return all feedback

--- feedbackHandlers.js
+++ feedbackHandlers.js
@@ -1,7 +1,8 @@
 const Feedback = require("./feedbackLib");
 
 const getAllFeedbacks = (req, res) => {
-  res.json({ message: "Hello from getAllFeedbacks" });
+  const feedbacks = Feedback.getAll();
+  res.json(feedbacks);
 };
 
 const createFeedback = (req, res) => {
